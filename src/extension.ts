0058f99004bf1b8196c0192f8c126bb0af026e7d
tylim <limteikyee@gmail.com>
2023-05-04 01:11:39 +0800

    remove comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 import convertPath from '@stdlib/utils-convert-path'
-// @ts-expect-error
-import ks from 'node-key-sender'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "folder-lock" is now active!')
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-
     const hide = vscode.commands.registerCommand(
         'folder-hide.hide',
         (uri: vscode.Uri) => {
@@ -39,9 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
     const unhide = vscode.commands.registerCommand(
         'folder-hide.unhide',
         async () => {
-            // Get the configuration target for the workspace
-            const configTarget = vscode.ConfigurationTarget.Workspace
-            // Open the workspace 'files.exclude' settings in a new tab
             await vscode.commands.executeCommand(
                 'workbench.action.openWorkspaceSettings',
                 { query: 'files.exclude' }
@@ -52,5 +35,4 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(hide, unhide)
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
